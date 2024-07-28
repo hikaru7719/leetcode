6e9ece5bab913888b6022147de88805151a1656b
hikaru7719 <j148016s@st.u-gakugei.ac.jp>
2024-07-28 13:07:23 +0900

    maximum subarray

---
+++ leetcode/maximum-subarray.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        maxSum = float("-inf")
+        currentSum = 0
+
+        for num in nums:
+            currentSum += num
+
+            if currentSum > maxSum:
+                maxSum = currentSum
+
+            if currentSum < 0:
+                currentSum = 0
+
+        return int(maxSum)
